c737d616f23f7c7a78e74e577876e7e3ff66befb
kanglerian <kanglerian@gmail.com>
2024-06-18 10:59:58 +0700

    first commit

---
+++ src/main/java/com/kanglerian/restful_api/ResTfulApiContactManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kanglerian.restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiContactManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiContactManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanglerian/restful_api/ResTfulApiContactManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanglerian.restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiContactManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
